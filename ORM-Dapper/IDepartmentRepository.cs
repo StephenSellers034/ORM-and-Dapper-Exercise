931aeb5ca022d63a5c6bc1fc258fc2fe5ade38c8
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-22 15:04:45 -0400

    Orm and Dapper Exercise

---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ORM_Dapper
+{
+	public interface IDepartmentRepository
+	{
+        public IEnumerable<Department> GetAllDepartments(); //Stubbed out method
+        public void InsertDepartment(string newDepartmentName);
+    }
+}
+
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+            var repo = new DapperDepartmentRepository(conn);
+
+            repo.InsertDepartment("Automotive");
+
+            Console.WriteLine("Type a new Department name");
+            var newDepartment = Console.ReadLine();
+
+            repo.InsertDepartment(newDepartment);
+
+            IEnumerable<Department> allDepartments = repo.GetAllDepartments();
+            foreach(Department item in allDepartments)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.DepartmentID);
+                Console.WriteLine();
+                 
+            }
+
         }
     }
 }
